e39b4531765ff1a549d954cbc92ae9a85362a645
Seyoung <seyoung5157@gmail.com>
2024-10-29 10:48:04 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+namespace InventoryFlow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "Data Source=C:\\study\\c#\\InventoryFlow\\inventory.db;Version=3;";
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                string selectQuery = "SELECT * FROM test;";
+
+                using (var command = new SQLiteCommand(selectQuery, connection))
+                {
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        StringBuilder message = new StringBuilder();
+
+                        while (reader.Read())
+                        {
+                            message.AppendLine($"값: {reader[0]}");
+                        }
+
+                        MessageBox.Show(message.ToString(), "Query Result", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+            }
+        }
+    }
+}
